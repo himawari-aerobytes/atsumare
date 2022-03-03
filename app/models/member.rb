f2e5df174b899ac06ec58ed8f31365f5e63021e5
n-syuichi <syuichi.nakajima.8t@stu.hosei.ac.jp>
2022-03-03 19:38:46 +0900

    Add Member Relation

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,8 @@
 class Member < ApplicationRecord
   has_many :events
-  has_one :division,:group,:schoolname,:zone
+  has_one :division
+  has_one :group
+  has_one :schoolname
+  has_one :zone
   has_many :events, through: :appointments
 end
